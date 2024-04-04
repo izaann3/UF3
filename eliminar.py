9bf83aa4ca1835f417cbd2e761c2ea2ef1c877cd
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-04-04 09:47:48 +0200

    hacemos el modelo top down

---
+++ eliminar.py
@@ -0,0 +1,15 @@
+import os
+
+'''
+1. Eliminar carpeta y su contenido
+2.1. Obtener el nombre de la carpeta
+2.2. Verificar la existencia de la carpeta
+2.3. Eliminar el contenido de la carpeta
+2.3.1. Obtener todos los elementos de la carpeta
+2.3.2. Iterar sobre los elementos
+2.3.3. Si es un archivo, eliminarlo
+2.3.4. Si es una carpeta, volvemos al paso 2.3.1
+2.4. Eliminar la carpeta vacía
+3.1. Pedir al usuario la ruta de la carpeta
+3.2. Eliminar la carpeta y su contenido
+'''
