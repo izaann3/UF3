faf38cb9457b622daaf0cf2634bfc844b877bc1e
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-04-04 08:19:45 +0200

    Apuntes primera clase

---
+++ import_os.py
@@ -0,0 +1,17 @@
+import os
+
+#r permite leer
+pd = open("/home/alumne/Escriptori/app.py","r")
+
+#w permite escribir
+pd = open("/home/alumne/Escriptori/app.py","w")
+
+#a permite hacer append
+pd = open("/home/alumne/Escriptori/app.py","a")
+
+#r+w permite leer + escribir
+pd = open("/home/alumne/Escriptori/app.py","r+w")
+
+os.listdir()
+
+print(pd)
