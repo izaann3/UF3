46f33ce100ced0fcaf63472ebb1c5600a0e261c7
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-04-04 08:51:29 +0200

    seguimos poniendo teoria

--- import_os.py
+++ import_os.py
@@ -15,4 +15,16 @@ fd = open("/home/alumne/Escriptori/app.py","r+")
 
 os.listdir()
 
+#read te leera el archivo
+fd.read()
+
+#seek 0,0 te esta diciendo que va a mover de 1 en 1 desde el inicio
+fd.seek(0,0)
+
+#seek 0,1 te esta diciendo que va a mover de 1 en 1 desde donde esta
+fd.seek(0,1)
+
+#seek 0,2 te esta diciendo que va a mover de 1 en 1 desde el final
+fd.seek(0,2)
+
 print(fd)
